ba63628fd099952f35da594c1a69fa0857119693
Stefan Kleineikenscheidt <stefan@k15t.com>
2016-02-29 20:10:47 +0100

    Added docs and package.json.

--- README.md
+++ README.md
@@ -3,33 +3,111 @@
 The Gulp plugin for Scroll Viewport uploads theme resources directly into Scroll Viewport.
 
 This is useful, when developing a Scroll Viewport theme locally. In this case, a Gulp file 
-can watch the resources, automatically upload the resources to Scroll Viewport, and have 
+can watch the resources, automatically upload the resources to Scroll Viewport, and even have 
 for example BrowserSync to sync the browser.
 
-## Usage
+## Set-up
 
-```
-gulp.task('js', function () {
-    return gulp.src('assets/js/**/*.*')
-        .pipe(viewport(defaultOpts))
-        .pipe(gulp.dest('build/js'));
-});
-```
+Create a config file ``~/.viewportrc`` that contains a list of all themes you want to upload to.
+
+    [DEV]
+    confluenceBaseUrl=http://localhost:1990/confluence
+    viewportUrl=http://localhost:1990/confluence/en
+    username=admin
+    password=admin
+    # Theme "My Theme"
+    themeId=C0A8B21601532D65BE2C643B2E3FB880
+    
+Each section in the file is represents a theme on a specific Confluence server, a so-called
+"target". In the example above there is one target called "DEV".                                                                    
+
+Then you can use the Gulp Viewport plugin in your gulp file.
+ 
+ 
+### Upload all files in pipeline
+
+The gulp-viewport plugin provides a special destination, that uploads a files in the 
+pipeline to a target (that has been defined in the ~/.viewportrc file). 
+
+    gulp.task('templates', function () {
+        return gulp.src('assets/**/*.vm')
+            .pipe(viewport.upload(TARGET, UPLOAD_OPTS))  // upload to viewport
+            .pipe(gulp.dest('build/templates'));         // also copy to local 'build' folder
+    }
+
+
+### Upload preprocessed file(s)
+
+Especially CSS and JS files usually need some batching, minification and other pre-processing.
+Here is how to do it.
+
+    gulp.task('less', function () {
+        return gulp.src('assets/less/main.less')
+            .pipe(gulpSourcemaps.init())
+            .pipe(gulpLess())
+            .pipe(minifyCss())
+            .pipe(viewport.upload(TARGET, {
+                targetPath: 'css/main.css',    // target destination of batched file
+                success: browserSync.reload    
+            }))
+            .pipe(gulp.dest('build/css'));
+    });
+
+
+### Set-up BrowserSync
+
+For development gulp-watch and BrowserSync is super handy.
+
+To set up gulp-watch and BrowserSync:
+
+    // Dependencies 
+    var browserSync = require('browser-sync').create();
+    [...]
+    var viewport = require('gulp-viewport');
+
+    // The target system needs to match with a section in .viewportrc
+    var TARGET = 'DEV';
+    
+    // The default upload opts.
+    var UPLOAD_OPTS = { sourceBase: 'assets' };
+    
+    gulp.task('watch', function () {
+    
+        // init browser sync.
+        browserSync.init({
+            open: false,
+            proxy: viewport.getViewportUrl('DEV')   // the target needs to define a viewportUrl
+        });
+    
+        // Override the UPLOAD_OPTS.
+        UPLOAD_OPTS = extend(DEFAULT_UPLOAD_OPTS, {
+            uploadOnlyUpdated: 'true',
+            success: browserSync.reload
+        });
+    
+        gulp.watch('assets/less/**.less', ['less']);
+        gulp.watch('assets/**/*.vm', ['templates']);
+        // ... create more watches for other file types here
+    });
+
+
+### Delete all files from theme
+
+    gulp.task('js', function () {
+        return gulp.src('assets/js/**/*.*')
+            .pipe(viewport(defaultOpts))
+            .pipe(gulp.dest('build/js'));
+    });
+
 
 ## Known Issues
 
-Scroll Viewport does have a bug with multiple parallel uploads 
-(https://k15t.jira.com/browse/VPRT-719). As a workaround, upload the theme initially by 
-hand, and then enable the throttling in line 25.
-
-## TODOs
-
-* Keep the local folder completely in sync with the remote folder, i.e. not only handle file changed events but also added/deleted/moved.
-* Use https://gist.github.com/xat/4b454edaf1949012ddee to work around the existing issues.
+* Scroll Viewport does have a bug with multiple parallel uploads (https://k15t.jira.com/browse/VPRT-719).
+* 
 
 ## Resources & Further Reading
 
 The following resources have been used when creating the plugin:
 
-* a general starter on gulp plugins: https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/guidelines.md
-* for the API of the file objects used here: https://github.com/wearefractal/vinyl
+* A general starter on Gulp plugins: https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/guidelines.md
+* For the API of the file objects used here: https://github.com/wearefractal/vinyl
---
+++ index.js
@@ -0,0 +1,170 @@
+"use strict";
+
+var clone = require('clone');
+var extend = require('extend');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var homeConfig = require('home-config');
+var path = require('path');
+var request = require('request');
+var strformat = require('strformat');
+var through = require("through2");
+
+
+const PLUGIN_NAME = 'gulp-viewport-uploader';
+const RC_FILENAME = '.viewportrc';
+
+const LOG_ENABLED = true;
+const DEBUG_ENABLED = false;
+
+const UPDATE_REST_URL = '/rest/scroll-viewport/1.0/theme/{0}/resource';
+const DELETE_REST_URL = '/rest/scroll-viewport/1.0/theme/{0}/resources';
+
+
+function log(msg, obj) {
+    if (LOG_ENABLED) {
+        console.log(msg + objAsString(obj));
+    }
+}
+
+
+function objAsString(obj) {
+    var dontLogContents = function (key, value) {
+        if (key == '_contents') {
+            return undefined;
+        } else {
+            return value;
+        }
+    };
+
+    if (obj) {
+        return ' ((' + JSON.stringify(obj, dontLogContents, 4) + '))';
+    } else {
+        return '';
+    }
+}
+
+
+function debug(msg, obj) {
+    if (DEBUG_ENABLED) {
+        console.log(msg + objAsString(obj));
+    }
+}
+
+
+function uploadPipeline(target, opts) {
+
+    var options = extend(true, getTargetConfig(target), opts);
+
+    if (!options.themeId) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'Missing options.themeId!');
+    }
+
+    if (!options.sourceBase && !options.targetPath) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'Missing options.sourceBase or options.targetPath!');
+    }
+
+    // Creating a stream through which each file will pass
+    return through.obj(function (file, enc, cb) {
+
+        if (file.isNull()) {
+            // return empty file
+            return cb(null, file);
+        }
+
+        var isOriginalUpdated = function (file) {
+            return (file.stat.atime.getTime() === file.stat.mtime.getTime())
+        };
+
+        var hasHistory = function (file) {
+            return (file.history.length > 1);
+        };
+
+        if (!opts.uploadOnlyUpdated || isOriginalUpdated(file) || hasHistory(file)) {
+            uploadFile(file, options);
+        } else {
+            debug('NOT uploading ' + file.history[0]);
+        }
+
+        cb(null, file);
+    });
+
+}
+
+
+function getTargetConfig(target) {
+    if (!homeConfig.load(RC_FILENAME).hasOwnProperty(target)) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'No configuration for target \'' + target + '\' found - check ~/.viewportrc.');
+    } else {
+        return homeConfig.load(RC_FILENAME)[target];
+    }
+}
+
+
+function uploadFile(file, opts) {
+    var options = opts;
+
+    if (!options.targetPath) {
+        var targetPath = path.relative(path.join(file.cwd, options.sourceBase), file.history[file.history.length - 1]);
+        targetPath = path.normalize(targetPath);
+    } else {
+        targetPath = options.targetPath;
+    }
+
+    log('Uploading \'' + file.history[0] + '\' to \'' + targetPath + '\'.');
+
+    file.pipe(request({
+        url: strformat(options.confluenceBaseUrl + UPDATE_REST_URL, options.themeId),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Content-Length': file.contents.length,
+            'X-Scroll-Viewport-Resource-Location': targetPath,
+            'Authorization': ('Basic ' + new Buffer(options.username + ':' + options.password).toString('base64'))
+        }
+
+    }, function (error, response) {
+        if (response.statusCode === 201) {
+            options.success && options.success(file, response);
+            log('SUCCESS: \'' + JSON.parse(response.body).name + '\' successfully uploaded.');
+
+        } else {
+            options.error && options.error(file, response);
+            log('ERROR:   Could not upload file to \'' + options.confluenceBaseUrl + '\':\n' +
+                response.statusCode + ' - ' + response.statusMessage);
+        }
+    }));
+
+}
+
+
+function removeAllResources(target) {
+
+    var options = getTargetConfig(target);
+
+    request({
+        url: strformat(options.confluenceBaseUrl + DELETE_REST_URL, options.themeId),
+        method: 'DELETE',
+        headers: {
+            'Authorization': ('Basic ' + new Buffer(options.username + ':' + options.password).toString('base64'))
+        }
+    });
+}
+
+
+function getViewportUrl(target) {
+    var targetConfig = getTargetConfig(target);
+
+    if (!targetConfig.hasOwnProperty('viewportUrl')) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'Property \'viewportUrl\' not set in target \'' + target + '\' - check ~/.viewportrc.');
+    }
+
+    return homeConfig.load(RC_FILENAME)[target].viewportUrl;
+}
+
+
+module.exports = {
+    'upload': uploadPipeline,
+    'removeAllResources': removeAllResources,
+    'getViewportUrl': getViewportUrl
+};
