d572f5705ec84f368d018fbdf96cfcec5b4c81a7
Martin Muzatko <martin@happy-css.com>
2017-07-05 15:16:09 +0200

    Rewrote plugin with class. Rewrite upload to use faster multipart upload

--- index.js
+++ index.js
@@ -1,213 +1,176 @@
-"use strict";
+"use strict"
+var fs = require('fs')
+var gutil = require('gulp-util')
+var homeConfig = require('home-config')
+var path = require('path')
+var request = require('request')
+var syncRequest = require('sync-request')
+var strformat = require('strformat')
+var through = require("through2")
+var observable = require("riot-observable")
 
-var clone = require('clone');
-var extend = require('extend');
-var fs = require('fs');
-var gutil = require('gulp-util');
-var homeConfig = require('home-config');
-var path = require('path');
-var request = require('request');
-var syncRequest = require('sync-request');
-var strformat = require('strformat');
-var through = require("through2");
+const PLUGIN_NAME = 'gulp-viewport'
+const VIEWPORTRC = homeConfig.load('.viewportrc')
 
+const THEME_BY_NAME_REST_URL = '/rest/scroll-viewport/1.0/theme?name={0}'
+const UPDATE_REST_URL = '/rest/scroll-viewport/1.0/theme/{0}/resource'
+const DELETE_REST_URL = '/rest/scroll-viewport/1.0/theme/{0}/resource'
 
-const PLUGIN_NAME = 'gulp-viewport-uploader';
-const VIEWPORTRC = homeConfig.load('.viewportrc');
-
-const THEME_BY_NAME_REST_URL = '/rest/scroll-viewport/1.0/theme?name={0}';
-const UPDATE_REST_URL = '/rest/scroll-viewport/1.0/theme/{0}/resource';
-const DELETE_REST_URL = '/rest/scroll-viewport/1.0/theme/{0}/resource';
-
-const LOG_ENABLED = true;
-const DEBUG_ENABLED = true;
+module.exports = class ViewportTheme {
+    constructor(options) {
 
 
-var ViewportTheme = function (themeName, targetSystemKey, uploadOpts) {
-    this.targetSystem = getTargetConfig(targetSystemKey);
-    this.themeId = getThemeId(this.targetSystem, themeName);
-    this.uploadOpts = uploadOpts;
+        // allow events to be triggered and listened to
+        observable(this)
 
-    log();
-    log('======================================================');
-    log('   You are deploying \'' + themeName + '\'to:');
-    log('   ' + this.targetSystem.confluenceBaseUrl);
-    log('======================================================');
-    log();
+        // Options are set via ENV variables, GULP Config and/or .viewportrc config
+        const defaultOptions = {
+            // If not existing, determine key using the id. one of the two has to exist
+            themeName: process.env.VPRT_THEMENAME,
+            // It is usually easier to use the themeName, but if needed - the id can be used.
+            themeId: process.env.VPRT_THEMEID,
+            // For home-config users of .viewportrc - defines which config to use for target
+            env: process.env.VPRT_ENV,
+            // If you do not want to use a .viewportrc file, you can manually pass the target opts
+            target: {
+                // https://your-installation.com/confluence/
+                confluenceBaseUrl: process.env.VPRT_CONFLUENCEBASEURL,
+                // A user that is eligible to update viewport themes in the given space
+                username: process.env.VPRT_USERNAME,
+                password: process.env.VPRT_PASSWORD,
+            },
+            // If the source is placed in a subfolder (dist/theme/...) use this path
+            sourceBase: process.env.VPRT_SOURCEBASE,
+            // If the source has to be placed somewhere else than /
+            targetPath: process.env.VPRT_TARGETPATH,
+        }
 
-    function getTargetConfig(targetSystemKey) {
-        if (process.env.hasOwnProperty('USER')
-            && process.env.hasOwnProperty('PASS')
-            && process.env.hasOwnProperty('URL')) {
-            var config = {
-                confluenceBaseUrl : process.env.URL,
-                username : process.env.USER,
-                password : process.env.PASS
-            };
-            return config;
-        } else if (!VIEWPORTRC.hasOwnProperty(targetSystemKey)) {
-            throw new gutil.PluginError(PLUGIN_NAME, 'No configuration for target \'' + targetSystemKey + '\' found - check ~/.viewportrc.');
-        } else {
-            return VIEWPORTRC[targetSystemKey];
+        options = Object.assign({}, defaultOptions, options)
+        options.target = Object.assign({}, defaultOptions.target, options.target)
+
+        this.options = this.validateOptions(options)
+
+        this.log(`${this.getUserAnnotation()} Changing theme ${gutil.colors.bold.red(this.options.themeName)} at ${gutil.colors.bold.green(this.options.target.confluenceBaseUrl)}`)
+
+        this.files = []
+    }
+
+    getUserAnnotation() {
+        if (this.options.env) {
+            return `[${gutil.colors.red.bold(this.options.target.username)}@${this.options.env}]`
+        } else if (this.options.target.confluenceBaseUrl) {
+            return `[${gutil.colors.red.bold(this.options.target.username)}]`
         }
     }
 
-    function getThemeId(targetSystem, themeName) {
-        var response = syncRequest('GET', strformat(targetSystem.confluenceBaseUrl + THEME_BY_NAME_REST_URL, themeName),
+    validateOptions(options) {
+        if (options.env && VIEWPORTRC.hasOwnProperty(options.env)) {
+            options.target = Object.assign({}, options.target, VIEWPORTRC[options.env])
+        }
+        if (!options.themeId) {
+            if (!options.themeName) {
+                throw new gutil.PluginError(PLUGIN_NAME, 'themeName or themeId missing')
+            } else {
+                options.themeId = this.getThemeId(options)
+            }
+        }
+        if (!options.targetPath) {
+            options.targetPath = './'
+        }
+        if (!options.sourceBase) {
+            options.sourceBase = './'
+        }
+        return options
+    }
+
+    getThemeId(options) {
+        var response = syncRequest('GET', strformat(options.target.confluenceBaseUrl + THEME_BY_NAME_REST_URL, options.themeName),
             {
                 headers: {
-                    'Authorization': ('Basic ' + new Buffer(targetSystem.username + ':' + targetSystem.password).toString('base64'))
+                    'Authorization': ('Basic ' + new Buffer(options.target.username + ':' + options.target.password).toString('base64'))
                 }
-            });
+            })
 
         if (response.statusCode != 200) {
-            throw new gutil.PluginError(PLUGIN_NAME, 'Theme \'' + themeName + '\' not found on \'' + targetSystem.confluenceBaseUrl +
-                '\'! Create a new theme named exactly like this to fix.');
+            throw new gutil.PluginError(PLUGIN_NAME, 'Theme \'' + options.themeName + '\' not found on \'' + options.target.confluenceBaseUrl +
+                '\'! Create a new theme named exactly like this to fix.')
         }
 
-        return JSON.parse(response.getBody('utf8')).id;
-    }
-};
-
-
-function upload(uploadOpts) {
-
-    var that = this;
-
-    // if we get upload opts here, clone existing and override
-    uploadOpts = uploadOpts ?
-        extend(clone(that.uploadOpts), uploadOpts) :
-        that.uploadOpts;
-
-    if (!this.themeId) {
-        throw new gutil.PluginError(PLUGIN_NAME, 'Missing options.themeId!');
+        return JSON.parse(response.getBody('utf8')).id
     }
 
-    if (!uploadOpts.sourceBase && !uploadOpts.targetPath) {
-        throw new gutil.PluginError(PLUGIN_NAME, 'Missing uploadOpts.sourceBase or uploadOpts.targetPath!');
+    log(message) {
+        // if we use the default console.log, objects and arrays are displayed nicely (though, wihout [plugin-name] branding)
+        // we have this function, so we can still change it to our liking
+        console.log(message)
     }
 
-    // Creating a stream through which each file will pass
-    return through.obj(function (file, enc, cb) {
-
-        if (file.isNull()) {
-            // return empty file
-            return cb(null, file);
-        }
-
-        uploadFile(file, that, uploadOpts, cb);
-
-        // cb(null, file);
-    });
-
-}
-
-
-function uploadFile(file, viewportTheme, uploadOpts, cb) {
-    if (!uploadOpts.targetPath) {
-        var targetPath = path.relative(path.join(file.cwd, uploadOpts.sourceBase), file.history[file.history.length - 1]);
-        targetPath = path.normalize(targetPath);
-    } else {
-        targetPath = uploadOpts.targetPath;
+    error(message) {
+        // if we use the default console.log, objects and arrays are displayed nicely (though, wihout [plugin-name] branding)
+        // we have this function, so we can still change it to our liking
+        console.error(message)
     }
 
-    debug('Uploading \'' + file.history[0] + '\' to \'' + targetPath + '\'.');
+    upload() {
+        this.trigger('upload')
+        return through.obj(
+            (file, enc, cb) => {
+                // If file exists, add it to our queue
+                if (!file.isNull()) {
+                    let relativePath = path.relative(this.options.targetPath, file.history[0])
+                    this.files.push(
+                        {
+                            path: path.relative(this.options.sourceBase, file.history[0]),
+                            file: fs.createReadStream(relativePath)
+                        }
+                    )
+                }
+                cb(null, file)
+            },
+            (cb) => {
+                let files = this.files.map(item=>item.file)
+                let locations = this.files.map(item=>item.path)
+                request({
+                    url: strformat(this.options.target.confluenceBaseUrl + UPDATE_REST_URL, this.options.themeId),
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Authorization': ('Basic ' + new Buffer(this.options.target.username + ':' + this.options.target.password).toString('base64'))
+                    },
+                    formData: { files, locations }
+                }, (error, response) => {
+                    if (response.statusCode === 201) {
+                        let uploadedFiles = JSON.parse(response.body)
+                        uploadedFiles.map(item=>this.log(item))
+                        this.log(`${uploadedFiles.length} Files ${gutil.colors.bold.green('successfully')} uploaded.`)
+                        this.trigger('uploaded')
+                    } else {
+                        this.error('Error while uploading files: ' + response.statusCode + ' - ' + response.statusMessage)
+                        this.trigger('error')
+                    }
+                    cb(null)
+                })
+            }
+        )
+    }
 
-    file.pipe(request({
-        url: strformat(viewportTheme.targetSystem.confluenceBaseUrl + UPDATE_REST_URL, viewportTheme.themeId),
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/octet-stream',
-            'Content-Length': file.contents.length,
-            'X-Scroll-Viewport-Resource-Location': targetPath,
-            'Authorization': ('Basic ' + new Buffer(viewportTheme.targetSystem.username + ':' + viewportTheme.targetSystem.password).toString('base64'))
-        }
-
-    }, function (error, response) {
-        if (response.statusCode === 201) {
-            uploadOpts.success && uploadOpts.success(file, response);
-            log('File \'' + JSON.parse(response.body).name + '\' successfully uploaded.');
-
-        } else {
-            uploadOpts.error && uploadOpts.error(file, response);
-            log('Error while uploading file to \'' + targetSystem.confluenceBaseUrl + '\':\n' +
-                response.statusCode + ' - ' + response.statusMessage);
-        }
-        cb(null, file)
-    }));
-
-}
-
-
-function extendUploadOpts(newUploadOpts) {
-    extend(this.uploadOpts, newUploadOpts);
-}
-
-
-function removeAllResources(uploadOpts) {
-
-    var that = this;
-
-    // if we get upload opts here, clone existing and override
-    uploadOpts = uploadOpts ?
-        extend(clone(that.uploadOpts), uploadOpts) :
-        that.uploadOpts;
-
-    request({
-        url: strformat(this.targetSystem.confluenceBaseUrl + DELETE_REST_URL, this.themeId),
-        method: 'DELETE',
-        headers: {
-            'Authorization': ('Basic ' + new Buffer(this.targetSystem.username + ':' + this.targetSystem.password).toString('base64'))
-        }
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 204) {
-            log('Resources succesfully removed.');
-        } else {
-            uploadOpts.error && uploadOpts.error(file, response);
-            log('Error while removing resources:\n' +
-                response.statusCode + ' - ' + response.statusMessage);
-        }
-    });
-}
-
-
-function log(msg, obj) {
-    if (LOG_ENABLED) {
-        if (msg) {
-            console.log(msg + objAsString(obj));
-        } else {
-            console.log();
-        }
+    removeAllResources() {
+        this.trigger('remove')
+        request({
+            url: strformat(this.options.target.confluenceBaseUrl + DELETE_REST_URL, this.options.themeId),
+            method: 'DELETE',
+            headers: {
+                'Authorization': ('Basic ' + new Buffer(this.options.target.username + ':' + this.options.target.password).toString('base64'))
+            }
+        }, (error, response, body) => {
+            if (!error && response.statusCode == 204) {
+                this.log('Resources succesfully removed.')
+                this.trigger('removed')
+            } else {
+                this.log('Error while removing resources:\n' +
+                response.statusCode + ' - ' + response.statusMessage)
+                this.trigger('error')
+            }
+        })
     }
 }
-
-
-function objAsString(obj) {
-    var dontLogContents = function (key, value) {
-        if (key == '_contents') {
-            return undefined;
-        } else {
-            return value;
-        }
-    };
-
-    if (obj) {
-        return ' ((' + JSON.stringify(obj, dontLogContents, 4) + '))';
-    } else {
-        return '';
-    }
-}
-
-
-function debug(msg, obj) {
-    if (DEBUG_ENABLED) {
-        console.log(msg + objAsString(obj));
-    }
-}
-
-
-ViewportTheme.prototype.upload = upload;
-ViewportTheme.prototype.extendUploadOpts = extendUploadOpts;
-ViewportTheme.prototype.removeAllResources = removeAllResources;
-
-module.exports = ViewportTheme;
